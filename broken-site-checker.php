abfe3f9418d8eb7db94165ae6ef8c6a818c62a07
Ryan Duff <ryan@fusionized.com>
2014-01-29 13:13:53 -0500

    Initial plugin w/ header

---
+++ broken-site-checker.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Plugin Name: Broken Site Checker
+    Plugin URI: http://maintainn.com
+    Description: Checks for and disables inaccessible sites in a multisite install
+    Author: Ryan Duff
+    Version: 0.1
+    Author URI: http://maintainn.com
+    Text Domain: maintainnbsc
+    Domain Path: /lang
+ */
+
