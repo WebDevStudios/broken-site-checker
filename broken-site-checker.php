f54bfbb1029ddb7181b76075ad4f5d3fe8605fc8
Ryan Duff <ryan@fusionized.com>
2014-01-30 11:43:15 -0500

    Fix plugin header indentation. Set version to 1.0

--- broken-site-checker.php
+++ broken-site-checker.php
@@ -1,12 +1,12 @@
 <?php
 /*
-    Plugin Name: Broken Site Checker
-    Plugin URI: http://maintainn.com
-    Description: Checks for and disables inaccessible sites in a multisite install
-    Author: Ryan Duff
-    Version: 0.1
-    Author URI: http://maintainn.com
-    Text Domain: maintainnbsc
-    Domain Path: /lang
+Plugin Name: Broken Site Checker
+Plugin URI: http://maintainn.com
+Description: Checks for and disables inaccessible sites in a multisite install
+Author: Ryan Duff
+Version: 1.0
+Author URI: http://maintainn.com
+Text Domain: maintainn-broken-site-checker
+Domain Path: /lang
  */
 
